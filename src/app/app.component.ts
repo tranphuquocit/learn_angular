febd7daff1410e59c445dd466303f0799246f05e
Zin <tranphuquocit@gmail.com>
2023-11-10 00:02:38 +0700

    Bai giang hom nay chi co nhu vay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,52 +7,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class AppComponent {
 
-  @Output() ketqua = new EventEmitter<number>();
-
-  title = '';
-  phepTinhNhan = '';
-  output = 0;
-  showCalculator = false;
-  showUser = false;
-
-  clickFunc(event: any) {
-    this.title += `${event} `;
-    console.log(this.title);
-  }
-
-  clickTinhToan(oper: any) {
-    this.phepTinhNhan += `${oper}`;
-
-    // ham tinh output
-    // this.output = eval(this.phepTinhNhan);
-    // console.log(this.output);
-
-    // this.phepTinhNhan = this.output.toString();
-  }
-
-  bang() {
-    this.output = eval(this.phepTinhNhan);  
-    this.phepTinhNhan = this.output.toString();
-  }
-  
-  delete() {
-    this.phepTinhNhan.slice(0, -1);
-    console.log(this.phepTinhNhan);
-  }
-
-  clear() {
-    this.phepTinhNhan = '';
-    console.log(this.phepTinhNhan); 
-  }
-
-  showCal() {
-    this.showCalculator = !this.showCalculator;
-    console.log(this.showCalculator);
-  }
-
-  showUserr() {
-    this.showUser = !this.showUser;
-    console.log(this.showUser);
-
-  }
+  public baCach: any;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { ColorContentComponent } from './component/color-content/color-content.component';
-import { BtnCalculatorComponent } from './component/btn-calculator/btn-calculator.component';
-import { RecordComponent } from './component/record/record.component';
 import { FormsModule } from '@angular/forms';
 
 const COMPONENT = [
-  ColorContentComponent,
-  BtnCalculatorComponent,
-  RecordComponent
 ]
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    ...COMPONENT
+    AppComponent
   ],
   imports: [
     BrowserModule,
