8cb60e978be40e992596c1120095410747e12764
davidtuan02 <tuantienti050222@gmail.com>
2023-12-11 12:33:52 +0700

    updated

--- src/app/components/input-chart/input-chart.component.ts
+++ src/app/components/input-chart/input-chart.component.ts
@@ -90,6 +90,71 @@ export class InputChartComponent {
 
         for(let itemInfoList of this.userInfoList) {
             for(let itemChartList of this.chartList) {
+                if(itemInfoList.age >= 1 && itemInfoList.age <= 10) {
+                    if(itemChartList.name == '1-10') {
+                        itemChartList.count = (itemChartList.count + 1);
+                    }
+                }
+                else if(itemInfoList.age > 10 && itemInfoList.age <= 20) {
+                    if(itemChartList.name == '11-20') {
+                        itemChartList.count = (itemChartList.count + 1);
+                    }
+                }
+                else if(itemInfoList.age > 20 && itemInfoList.age <= 30) {
+                    if(itemChartList.name == '21-30') {
+                        itemChartList.count = (itemChartList.count + 1);
+                    }
+                }
+                else if(itemInfoList.age > 30 && itemInfoList.age <= 40) {
+                    if(itemChartList.name == '31-40') {
+                        itemChartList.count = (itemChartList.count + 1);
+                    }
+                }
+                else if(itemInfoList.age > 40 && itemInfoList.age <= 50) {
+                    if(itemChartList.name == '41-50') {
+                        itemChartList.count = (itemChartList.count + 1);
+                    }
+                }
+                else if(itemInfoList.age > 50) {
+                    if(itemChartList.name == '51+') {
+                        itemChartList.count = (itemChartList.count + 1);
+                    }
+                }
+            }
+        }
+
+        for(let item of this.chartList) {
+            item.percent = item.count/this.totalUser*300 + 'px';
+            // console.log(item.percent)
+        }
+    }
+
+    public updateInfo(index: number) {
+        this.userInfo = {
+            name: this.userInfoList[index].name,
+            age: this.userInfoList[index].age,
+            sex: null,
+            phone: null,
+            address: null
+        }
+        this.indexSaveInfo = index;
+        // this.saveUserInfo(index);
+    }
+
+    public deleteInfo(index: number) {
+        this.userInfoList.splice(index, 1);//xoa phan tu trong mang vi tri index
+        this.indexSaveInfo = this.indexSaveInfo - 1;//set index lui di 1
+        // console.log(this.userInfoList)
+        // console.log(this.userInfoList[index])
+        this.totalUser = this.userInfoList.length;
+        // console.log(this.totalUser);
+        
+        for(let item of this.chartList) {
+            item.count = 0;
+        }
+
+        for(let itemInfoList of this.userInfoList) {
+            for(let itemChartList of this.chartList) {
                 if(itemInfoList.age > 1 && itemInfoList.age <= 10) {
                     if(itemChartList.name == '1-10') {
                         itemChartList.count = (itemChartList.count + 1);
@@ -125,25 +190,14 @@ export class InputChartComponent {
 
         for(let item of this.chartList) {
             item.percent = item.count/this.totalUser*300 + 'px';
-            console.log(item.percent)
+            // console.log(item.percent);
         }
-    }
 
-    public updateInfo(index: number) {
-        this.userInfo = {
-            name: this.userInfoList[index].name,
-            age: this.userInfoList[index].age,
-            sex: null,
-            phone: null,
-            address: null
+        if(this.totalUser == 0) {
+            for(let item of this.chartList) {
+                item.percent = '0';
+            }
         }
-        this.indexSaveInfo = index;
-        // this.saveUserInfo(index);
-    }
-
-    public deleteInfo(index: number) {
-        this.userInfoList.splice(index, 1);//xoa phan tu trong mang vi tri index
-        this.indexSaveInfo = this.indexSaveInfo - 1;//set index lui di 1
-        console.log(this.userInfoList);
+        // console.log(this.chartList);
     }
 }
