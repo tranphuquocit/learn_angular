36a505e0978b78bb021f37ab67c629fee80c9421
davidtuan02 <tuantienti050222@gmail.com>
2024-01-14 18:01:32 +0700

    done

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,19 +1,35 @@
-import { Component , Input} from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { ItemModel } from "src/app/models/item.model";
 
 @Component({
-    selector: 'app-table',
-    templateUrl: './table.component.html',
-    styleUrls: ['./table.component.scss']
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
 })
 
 export class TableComponent {
 
-    @Input() listItem!: ItemModel[];
+  public item!: ItemModel;
+  public tempItem!: ItemModel;
+  @Input() listItem!: ItemModel[];
 
-    constructor() {
-        // console.log(this.listItem)
-    }
-    ngOnInit() {}
+  constructor() {}
+  ngOnInit() { }
 
+  public editItem(item: ItemModel) {
+    item.isEdit = true;
+    this.tempItem = JSON.parse(JSON.stringify(item));
+  }
+
+  public cancelEditItem(item: ItemModel) {
+    item.content = this.tempItem.content;
+    item.level = this.tempItem.level;
+    item.isEdit = false;
+  }
+
+  public saveEditItem(item: ItemModel) {
+    item.isEdit = false;
+    item.content = this.item.content;
+    item.level = this.item.level;
+  }
 }
